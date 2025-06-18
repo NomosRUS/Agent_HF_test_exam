120ba11f5761a9c92938a8de29063891dbaf2ee2
Sergey Nakvasin <NomosRUS@users.noreply.huggingface.co>
2025-06-18 11:15:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,12 @@ class BasicAgent:
     SYSTEM_PROMPT = (
         "You are a general AI assistant. I will ask you a question. "
         "Report your thoughts, and finish your answer with the following template: "
-        "FINAL ANSWER: [YOUR FINAL ANSWER]. YOUR FINAL ANSWER should be a number OR as few words as possible OR a comma separated list of numbers and/or strings. "
+        "FINAL ANSWER: [YOUR FINAL ANSWER]" 
+        "YOUR FINAL ANSWER should be a number OR as few words as possible OR a comma separated list of numbers and/or strings. "
         "If you are asked for a number, don't use comma to write your number neither use units such as $ or percent sign unless specified otherwise. "
         "If you are asked for a string, don't use articles, neither abbreviations, and write the digits in plain text unless specified otherwise. "
         "If you are asked for a comma separated list, apply the above rules depending of whether the element to be put in the list is a number or a string."
+        "Always end your output exactly with FINAL ANSWER: [YOUR FINAL ANSWER] and do not add any text after that."
     )
     
     #def __init__(self, model_name="google/flan-t5-base"):
